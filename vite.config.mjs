27cdb6b1613c92d13f1e41690330ad478a78ccf2
iamtunji <tunjitaiwo@cyberkonsults.com>
2025-04-29 00:09:07 -0400

    dfdf

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+//const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   esbuild: {
